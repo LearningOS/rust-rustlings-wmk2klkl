a6c8178b57f758d2cc8facbf6383ea3503540814
wmk2klkl <145705377+wmk2klkl@users.noreply.github.com>
2023-10-17 05:11:46 +0000

    update2

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,16 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
+// 在任何给定时间，要么只能有一个可变引用，要么可以有多个不可变引用。
+
+// 不可变引用和可变引用之间是互斥的，不能同时存在。
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
+    
     *z += 1000;
     assert_eq!(x, 1200);
 }
